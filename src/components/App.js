48bc997242ee083784738d29c272c70fd0a90909
arjundevlop <arjun.tomar45@gmail.com>
2023-05-01 17:14:02 +0530

    submitN

--- src/components/App.js
+++ src/components/App.js
@@ -4,38 +4,44 @@ import './../styles/App.css';
 import { useState } from "react";
 
 const App = () => {
-
-  const [todos, settodos] = useState([
-    {id:1, title:"Learn React",button:true},
-    {id:2, title:"Build a React app",button:true},
-    {id:3, title:"Deploy the React app",button:true}
-  ])
-
-  const removeTodo = (id) =>{
-    let objToUpdate = todos.find(obj => obj.id === id);
-    
-    objToUpdate.button = false;
-    
-
-    let updatedtodos = todos.map(obj => obj.id === id ? objToUpdate : obj);
-    settodos(updatedtodos)
-    
+  const [todos, setTodos] = useState([
+  { id: 1, title: "Learn React", button: true },
+  { id: 2, title: "Build a React app", button: true },
+  { id: 3, title: "Deploy the React app", button: true }
+  ]);
+  
+  const removeTodo = (id) => {
+  const updatedTodos = todos.map((todo) => {
+  if (todo.id === id) {
+  return { ...todo, button: false };
+  } else {
+  return todo;
   }
+  });
+  
+  setTodos(updatedTodos);
+  };
+  
   return (
-    <div>
-        <div className="parent">
-          <h1>Parent Component</h1>
-        </div>
-        <div className="child">
-          <h2>Child Component</h2>
-          <ul>
-            {todos.map((item)=>{
-              return <li>{item.title} {item.button ? <button onClick={()=>{removeTodo(item.id)}}>Complete</button>:""}</li>
-            })}
-          </ul>
-        </div>
-    </div>
-  )
-}
+  <div>
+  <div className="parent">
+  <h1>Parent Component</h1>
+  </div>
+  <div className="child">
+  <h2>Child Component</h2>
+  <ul>
+  {todos.map((todo) => (
+  <li key={todo.id}>
+  {todo.title}{" "}
+  {todo.button ? (
+  <button onClick={() => removeTodo(todo.id)}>Complete</button>
+  ) : null}
+  </li>
+  ))}
+  </ul>
+  </div>
+  </div>
+  );
+  };
 
 export default App
